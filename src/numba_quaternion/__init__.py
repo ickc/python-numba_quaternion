6a167788c85b8fd895be414564d7be7652a9e6c0
Kolen Cheung <christian.kolen@gmail.com>
2021-03-18 20:15:48 -0700

    rename angular_position_and_orientation to azimuthal_equidistant_projection_with_orientation

--- src/numba_quaternion/__init__.py
+++ src/numba_quaternion/__init__.py
@@ -120,8 +120,8 @@ def rotate(p: np.ndarray[np.complex_], v: np.ndarray[np.complex_]) -> np.ndarray
 
 
 @jit(nopython=True, nogil=True)
-def quat_to_angular_position_and_orientation(quats: np.ndarray[np.complex_]) -> np.ndarray[np.float_]:
-    """Convert from detector pointing to angular location and orientation.
+def quat_to_azimuthal_equidistant_projection_with_orientation(quats: np.ndarray[np.complex_]) -> np.ndarray[np.float_]:
+    """Convert from detector pointing to Azimuthal equidistant projection with orientation.
 
     Returned array is in radian,
     has the last dimension with 3 elements,
@@ -190,8 +190,16 @@ class Quaternion:
         return Quaternion.from_array_complex(rotate(self.array_complex, other.array_complex))
 
     @cached_property
-    def angular_position_and_orientation(self) -> np.ndarray[np.float_]:
-        return quat_to_angular_position_and_orientation(self.array_complex)
+    def azimuthal_equidistant_projection_with_orientation(self) -> np.ndarray[np.float_]:
+        """Convert from detector pointing to Azimuthal equidistant projection with orientation.
+
+        Returned array is in radian,
+        has the last dimension with 3 elements,
+        1st as the horizontal angular position,
+        2nd as the vertical angular position,
+        3rd as the orientation in angle.
+        """
+        return quat_to_azimuthal_equidistant_projection_with_orientation(self.array_complex)
 
     @classmethod
     def from_array_complex(cls, array_complex: np.ndarray[np.complex_]) -> Quaternion:
